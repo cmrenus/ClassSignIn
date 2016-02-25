a58447a58b7e14a57136b44f13e2ad33cb1637db
cmrenus <cmrenus@gmail.com>
2016-02-24 20:05:18 -0500

    Cross site error, works in developer mode

--- client/controllers/homeCtrl.js
+++ client/controllers/homeCtrl.js
@@ -1,14 +1,15 @@
 
 
 angular.module('ClassSignIn')
-.controller('homeCtrl', ['$scope', '$http', function($scope, $http) {
+.controller('homeCtrl', ['$scope', '$http', '$window', function($scope, $http, $window) {
 	$scope.login = function(){
 		$http({
 			method : 'GET',
 			url: '/login',
 			withCredentials: true
-		}).then(function(response){
-			console.log(response);
+		}).then(function(data){
+			$window.location = 'https://cas-auth.rpi.edu/cas/login?service=http%3A%2F%2Flocalhost%3A8005%2Flogin&renew=false';
+			console.log(data);
 		}).catch(function(err){
 			console.log(err);
 		});
--- server.js
+++ server.js
@@ -38,44 +38,20 @@ var cas = new CASAuthentication({
     cas_version: '2.0'
 });
 
-
 app.use('/', routes);
 
 
 app.get('/login', cas.bounce, function (req, res) {
-	console.log(res);
-  	/*if (!req.session || !req.session.cas_user) {
-        res.redirect('/logout');
-    }
-    res.send( '<html><body>Hello!</body></html>' );*/
-    /*var rcs_id = req.session.cas_user.toLowerCase();
-    console.log(rcs_id);*/
-    /*
-    Q.all([
-        cms.getWTG(rcs_id),
-        cms.getRNE(rcs_id)
-    ]).then(function (responses) {
-        var wtg_status = JSON.parse(responses[0]).result,
-            rne_status = JSON.parse(responses[1]).result;
-
-        if(wtg_status || rne_status) {
-            var logger_desc = "Granted admin access by CMS for ";
-            if(wtg_status) logger_desc += "WTG ";
-            if(wtg_status && rne_status) logger_desc += "and ";
-            if(rne_status) logger_desc += "RNE ";
-            logger_desc += "membership";
-
-            custom_logger.write(null, req.session.cas_user, 'CMS_ADMIN', logger_desc);
-        }
-
-        req.session.admin_rights = wtg_status || rne_status;
-        req.session.is_authenticated = true;
-
+  	if (!req.session || !req.session.cas_user) {
         res.redirect('/');
-    });*/
+    }
+    console.log(req.session);
+    res.redirect('/');
 });
 
 app.get('/logout', cas.logout);
+
+
 
 
 app.listen(port);
